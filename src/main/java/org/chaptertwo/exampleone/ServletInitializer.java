175c2ca8c9a5718646b5c975bc51e294f66f84ac
ramanamuttana <ramanamuttana@gmail.com>
2023-01-09 22:47:33 +0100

    bump java 1.8 to java 11

--- src/main/java/org/chaptertwo/exampleone/ServletInitializer.java
+++ src/main/java/org/chaptertwo/exampleone/ServletInitializer.java
@@ -3,7 +3,7 @@ package org.chaptertwo.exampleone;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+public abstract class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
