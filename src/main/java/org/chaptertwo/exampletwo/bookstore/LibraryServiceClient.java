a7d26843675557f5f392e9be9d5ad080c5a526d3
ramanamuttana <ramanamuttana@gmail.com>
2023-01-11 00:14:06 +0100

    removed jars from pom.xml ,added new code on client

---
+++ src/main/java/org/chaptertwo/exampletwo/bookstore/LibraryServiceClient.java
@@ -0,0 +1,23 @@
+package org.chaptertwo.exampletwo.bookstore;
+
+
+import java.util.logging.Logger;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.glassfish.jersey.jsonb.JsonBindingFeature;
+
+public class LibraryServiceClient {
+	private static final Logger LOGGER = Logger.getAnonymousLogger();
+
+	public static void main(String[] args) {
+// construct a JAX-RS client using the builder
+		Client client = ClientBuilder.newBuilder()
+				.register(JsonBindingFeature.class).build();
+// construct a web target for the library service
+		WebTarget api = client.target("http://localhost:8080").path("/library-service/api");
+
+	}
+}
