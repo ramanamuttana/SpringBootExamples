274ede50f6d58ed9b7c1f852e57d65acc2dd397a
ramanamuttana <ramanamuttana@gmail.com>
2023-02-22 23:48:05 +0100

    removed unwanted imports

--- src/main/java/org/chaptertwo/exampletwo/bookstore/BookResource.java
+++ src/main/java/org/chaptertwo/exampletwo/bookstore/BookResource.java
@@ -6,7 +6,6 @@ import java.util.Objects;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
