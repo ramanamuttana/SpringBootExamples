7d36716849c0bac8fc160e7a413bd2bfa284e8f9
Venkata Muttana <ramanamuttana@gmail.com>
2024-11-14 14:13:43 +0100

    Create  OrderService.java

---
+++ BasicSpringSetupExample/src/main/java/com/example/demo/service/OrderService.java
@@ -0,0 +1,32 @@
+package com.example.orderservice.service;
+
+import com.example.orderservice.model.Order;
+import com.example.orderservice.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public Order createOrder(Order order) {
+        return orderRepository.save(order);
+    }
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public Optional<Order> getOrderById(Long id) {
+        return orderRepository.findById(id);
+    }
+
+    public void deleteOrder(Long id) {
+        orderRepository.deleteById(id);
+    }
+}
